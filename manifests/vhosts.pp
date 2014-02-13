acfaef0537feb04c0cce6c6578420f62d76a0ee4
Raymond Kristiansen <raymond.kristiansen@it.uib.no>
2014-02-13 14:50:29 +0100

    Added new define to add new vhosts

---
+++ manifests/vhosts.pp
@@ -0,0 +1,38 @@
+# == Class: httpd::vhosts
+#
+# Add a new vhost to vhosts.d
+#
+# === Parameters
+#
+# [*service_name*]
+#
+# [*doc_root*]
+#
+#
+# === Examples
+#
+#
+# === Authors
+#
+# Raymond Kristiansen <st02221@uib.no>
+#
+# === Copyright
+#
+# Copyright 2014 UiB
+#
+define httpd::vhosts(
+  $service_name,
+  $doc_root,
+  $use_ssl = false,
+  $type = 'base',
+  $settings = ''
+) {
+
+  file { "${name}.conf":
+    path    => "/etc/httpd/vhosts.d/${name}.conf",
+    ensure  => present,
+    content => template("${module_name}/vhosts.d/${$type}.conf.erb"),
+    notify  => Class['httpd::service'],
+  }
+
+}
